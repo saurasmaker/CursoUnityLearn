107ef99c2ac9bf3ed6622f6138e7b74c7f4d93b1
Iván <48927047+saurasmaker@users.noreply.github.com>
2021-02-08 17:54:47 +0100

    2 init

---
+++ Unity Foundations/Assets/Scripts/HelloWorld.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    public string myMessage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        print(myMessage);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        print("Hello World in Update");
+    }
+}
