9c2cdebe3fe6e9d159de24270b47f8b3345a2984
Iván <48927047+saurasmaker@users.noreply.github.com>
2021-02-09 18:14:33 +0100

    lessons 4.1 & 4.2

---
+++ Prototype4/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rb;
+    private GameObject focalPoint;
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        focalPoint = GameObject.Find("FocalPoint");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float forwardInput = Input.GetAxis("Vertical");
+        rb.AddForce(focalPoint.transform.forward * speed * forwardInput * Time.deltaTime);
+    }
+}
