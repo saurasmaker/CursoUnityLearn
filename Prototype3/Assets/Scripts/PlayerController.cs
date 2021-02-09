9d1f5d0d278f4512f39f40416ed14a231e6573b7
Iván <48927047+saurasmaker@users.noreply.github.com>
2021-02-09 11:25:23 +0100

    lesson 3.4

--- Prototype3/Assets/Scripts/PlayerController.cs
+++ Prototype3/Assets/Scripts/PlayerController.cs
@@ -6,14 +6,21 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody rb;
     private Animator animator;
+    private AudioSource audioSource;
 
+    [SerializeField] private ParticleSystem explosionParticles;
+    [SerializeField] private ParticleSystem dirtParticles;
     [SerializeField] private float jumpForce;
     [SerializeField] private float gravityModifier;
+
+    public AudioClip jumpSound, crashSound;
+
 
     [HideInInspector] public bool isOnGround, gameOver;
 
     private void Awake()
     {
+        audioSource = GetComponent<AudioSource>();
         animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody>();
         Physics.gravity *= gravityModifier;
@@ -27,19 +34,28 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         if (!gameOver && isOnGround && Input.GetButtonDown("Jump"))
+        {
             rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+            audioSource.PlayOneShot(jumpSound, 1.0f);
+        }
     }
 
     private void OnCollisionEnter(Collision collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
+        {
             isOnGround = true;
+            dirtParticles.Play();
+        }
 
         else if (collision.gameObject.CompareTag("Obstacle"))
         {
             gameOver = true;
             animator.SetBool("Death_b", true);
             animator.SetInteger("DeathType_int", 1);
+            dirtParticles.Stop();
+            explosionParticles.Play();
+            audioSource.PlayOneShot(crashSound, 1.0f);
             Debug.Log("Game Over!");
         }
     }
@@ -48,6 +64,7 @@ public class PlayerController : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Ground"))
         {
+            dirtParticles.Stop();
             animator.SetTrigger("Jump_trig");
             isOnGround = false;
         }
