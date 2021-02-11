4692f471f7126bdc8eceb32cddff27e6cf8bdadc
Iván <48927047+saurasmaker@users.noreply.github.com>
2021-02-11 01:21:38 +0100

    Simple FUnctionality ended

---
+++ DebugTheApp/Assets/CongratScript.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CongratScript : MonoBehaviour
+{
+    public TextMesh text;
+    public ParticleSystem sparksParticles;
+
+    private List<string> textToDisplay;
+
+    private float rotatingSpeed;
+    private float timeToNextText;
+
+    private int currentText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        timeToNextText = 0.0f;
+        currentText = 0;
+
+        rotatingSpeed = 1.0f;
+
+        textToDisplay = new List<string>();
+        textToDisplay.Add("Congratulation");
+        textToDisplay.Add("All Errors Fixed");
+
+        text.text = textToDisplay[0];
+
+        sparksParticles.Play();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeToNextText += Time.deltaTime;
+        print(timeToNextText);
+        if (timeToNextText > 1.5f)
+        {
+            timeToNextText = 0.0f;
+
+            currentText++;
+            if (currentText < textToDisplay.Count)
+            {
+                text.text = textToDisplay[currentText];
+            }
+        }
+    }
+}
--- Prototype5/Assets/Challenge 5/Scripts/GameManagerX.cs
+++ Prototype5/Assets/Challenge 5/Scripts/GameManagerX.cs
@@ -9,8 +9,10 @@ public class GameManagerX : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
+    public TextMeshProUGUI durationText;
     public GameObject titleScreen;
-    public Button restartButton; 
+    public Button restartButton;
+    public int gameDuration;
 
     public List<GameObject> targetPrefabs;
 
@@ -23,13 +25,14 @@ public class GameManagerX : MonoBehaviour
     private float minValueY = -3.75f; //  y value of the center of the bottom-most square
     
     // Start the game, remove title screen, reset score, and adjust spawnRate based on difficulty button clicked
-    public void StartGame()
+    public void StartGame(int difficulty)
     {
-        spawnRate /= 5;
         isGameActive = true;
         StartCoroutine(SpawnTarget());
+        StartCoroutine(GameDuration());
         score = 0;
         UpdateScore(0);
+        spawnRate /= difficulty;
         titleScreen.SetActive(false);
     }
 
@@ -47,6 +50,18 @@ public class GameManagerX : MonoBehaviour
             }
             
         }
+    }
+
+    IEnumerator GameDuration()
+    {
+        for(int i = gameDuration; i >= -1 && isGameActive; --i)
+        {
+            yield return new WaitForSeconds(1f);
+            durationText.text = i + " seconds";
+        }
+        durationText.text = 0 + " seconds";
+        GameOver();
+        yield return null;
     }
 
     // Generate a random spawn position based on a random index from 0 to 3
@@ -70,14 +85,14 @@ public class GameManagerX : MonoBehaviour
     public void UpdateScore(int scoreToAdd)
     {
         score += scoreToAdd;
-        scoreText.text = "score";
+        scoreText.text = "score: " + score;
     }
 
     // Stop game, bring up game over text and restart button
     public void GameOver()
     {
         gameOverText.gameObject.SetActive(true);
-        restartButton.gameObject.SetActive(false);
+        restartButton.gameObject.SetActive(true);
         isGameActive = false;
     }
 
